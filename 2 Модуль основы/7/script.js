875e241686b59c1404d2eb6a5316044debe70c64
eyrieh <khrustalevnika@gmail.com>
2022-01-15 14:09:50 +0800

    ну вот

--- 2 Модуль основы/7/script.js
+++ 2 Модуль основы/7/script.js
@@ -6,3 +6,10 @@
 // const answer = prompt("сколько будешь?","10")
 console.log(typeof(answer)) // любая информация от пользователя будет в виде строк, "+" преобразует строку в число (пытается) попытка преобразовать символы к числу образует NaN
 console.log(1)
+
+let answers = []
+answers[0] = prompt("how old are you")
+answers[1] = prompt("where are you from")
+answers[2] = prompt("what is your name")
+
+document.write(answers)
