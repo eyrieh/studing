5dddaee069b2d47a2568bbe1785a2cfd7696590b
eyrieh <khrustalevnika@gmail.com>
2022-01-15 14:14:20 +0800

    какЗадаватьВопросыВБраузере

--- 2 Модуль основы/7какЗадаватьВопросыВБраузере/script.js
+++ 2 Модуль основы/7какЗадаватьВопросыВБраузере/script.js
@@ -7,9 +7,10 @@
 console.log(typeof(answer)) // любая информация от пользователя будет в виде строк, "+" преобразует строку в число (пытается) попытка преобразовать символы к числу образует NaN
 console.log(1)
 
-let answers = []
-answers[0] = prompt("how old are you")
+let answers = []//задаем переменной answers значение пустого массива
+answers[0] = prompt("how old are you")//задаем три элемента массива с тремя разными значениями
 answers[1] = prompt("where are you from")
-answers[2] = prompt("what is your name")
-
+answers[2] = prompt("what is your name") 
+//answers - массив [] - обращение к массиву, цифры - индекс элементов массива
+//answers[2] - обращение к элементу массива под индексом 2
 document.write(answers)
