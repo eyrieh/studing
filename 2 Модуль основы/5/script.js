87d9b4560812595926f54921706dec2210c4dcc9
eyrieh <khrustalevnika@gmail.com>
2022-01-15 13:10:04 +0800

    прошел 4,5,6 уроки

---
+++ 2 Модуль основы/5/script.js
@@ -0,0 +1,12 @@
+let number = 5;
+//Лет - объявление переменной, number - имя переменной, = - знак присваивания, 5 - значение переменной
+const one = 10;
+
+number = 10;
+console.log(number);
+const obj ={
+   a:50
+};
+obj.a = 100;
+obj = 1
+console.log(obj);
---
+++ 2 Модуль основы/6 знакомсивр с типами/script.js
@@ -0,0 +1,25 @@
+let number = 5.3; //число там
+const one = 1;
+
+console.log(4/0)
+
+let maxim = 1
+let chel = maxim //если скобок нет и нет переменной = смысла не имеет, но если скобки есть = строка (имеет смысол)
+let bool = (false) //булиновое значение//
+
+console.log(alex) //если мы пытаемся выбрать переменную, которой нет, но выдаст сообщениме is not defind
+
+let box;
+console.log(box) //если мы не задаем значение переменной, то выдаст сообщение underfind
+
+const nigger = {
+   owner: 'chel',
+   age: 99,
+   black: true       //<------объект {}
+}
+// console.log(nigger.age)
+
+let white = nigger.age //white - переменная со значением nigger.age
+console.log(white) //nigger.age - свойство объекта
+let snow =['snejinka', 'snejok', 'grad']; //<---- MASSIVE ARRAY
+console.log(snow[1])// тут ставятся квадратные скобки
