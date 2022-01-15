909a2f98e8ef87c943183efd760a5bb79995d0ee
eyrieh <khrustalevnika@gmail.com>
2022-01-15 14:57:23 +0800

    серега озадачил

---
+++ 2 Модуль основы/8шаблоннаяСтрока/script.js
@@ -0,0 +1,13 @@
+// const category = 'toys';
+// console.log(`http://somesite/${'toys'}/5`)
+
+// let answer = prompt("как тебя зовут?")
+// console.log(`ку, ${answer}`)
+
+const answers = []
+answers[0] = prompt('сколько лет?')
+answers[1] = prompt('негр?')
+answers[2] = prompt('пидор?')
+
+console.log(`лет - ${answers[0]}, негр - ${answers[1]}, пидор - ${answers[2]}`) 
+// вывел в консоль результат ответа пользователя под разными индексами
